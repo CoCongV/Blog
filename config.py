ecfc10be459045d60e6e8044a700a97bdafbaaa7
lvcong <cong.lv.yx@gmail.com>
2017-02-03 22:52:30 +0800

    set config

---
+++ config.py
@@ -0,0 +1,68 @@
+# coding: utf-8
+import os
+base = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECURET_KEY = os.environ.get('SECRET_KEY')
+    SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    FLASK_MAIL_SUBJECT_PREFIX = '[Cong\' Blog]'
+    FLASK_MAIL_SENDER = 'Admin <cong.lv@outlook.com>'
+    FLASK_ADMIN = os.environ.get('BLOG_ADMIN')
+    MAIL_SERVER = 'smtp-mail.outlook.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLE = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    BLOG_POST_PER_PAGE = 10
+    BLOG_COMMENT_PAGE = 30
+    BLOG_SLOW_DB_QUERY_TIME = 0.5
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost:3306/Flask_Blog_DEV'
+
+
+class TestingConfig(Config):
+    WTF_CSRF_ENABLE = False
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost:3306/Flask_Blog'
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:password@localhost:3306/Flask_Blog'
+
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials = None
+        secure = None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MIAL_USE_TLS', None):
+                secure = ()
+        mail_handler = StandardError(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            formaddr=cls.FLASK_MAIL_SENDER,
+            toaddr=cls.FLASK_ADMIN,
+            subject=cls.FLASK_MAIL_SUBJECT_PREFIX + ' Application Error',
+            credentials=credentials,
+            secure=secure
+        )
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
