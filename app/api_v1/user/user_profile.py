f42970facd3dd393262aeaca4578f7662ff506fe
Lv Cong <cong.lv.yx@gamil.com>
2018-01-01 23:23:14 +0800

    fix user profile bug

--- app/api_v1/user/user_profile.py
+++ app/api_v1/user/user_profile.py
@@ -1,4 +1,4 @@
-from flask import g
+from flask import g, request
 from flask_restful import Resource
 
 from app.models import User, Permission
@@ -12,7 +12,7 @@ class UserProfile(Resource, HTTPStatusCodeMixin):
 
     def get(self):
         # 权限分离
-        user = User.get(g.current_user.id)
+        user = User.get(request.args.get('uid'))
         edit_permission = False
         if g.current_user == user or g.current_user.can(Permission.ADMINISTER):
             edit_permission = True
