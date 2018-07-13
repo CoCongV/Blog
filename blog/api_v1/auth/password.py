6a11dc4928eea8fb76faffc8769ad9358a33b262
Lvcong <lv.cong@ricequant.com>
2018-07-13 15:57:24 +0800

    [fix] reqparse simple object

--- blog/api_v1/auth/password.py
+++ blog/api_v1/auth/password.py
@@ -5,28 +5,26 @@ from blog.api_v1 import token_auth
 from blog.api_v1.decorators import permission_required
 from blog.errors import AuthorizedError
 from blog.models import Permission
-from blog.utils.web import HTTPStatusCodeMixin
 
 
-class Password(Resource, HTTPStatusCodeMixin):
+reparse = reqparse.RequestParser()
+reqparse.add_argument(
+    'old_password', location='json', required=True)
+reqparse.add_argument(
+    'new_password', location='json', required=True)
+
+
+class Password(Resource):
 
     decorators = [permission_required(Permission.COMMENT),
                   token_auth.login_required]
 
-    def __init__(self):
-        super(Password, self).__init__()
-        self._reqparse = reqparse.RequestParser()
-
     def post(self):
-        self._reqparse.add_argument(
-            'old_password', location='json', required=True)
-        self._reqparse.add_argument(
-            'new_password', location='json', required=True)
-        args = self._reqparse.parse_args()
+        args = reqparse.parse_args()
         old_password = args['old_password']
         new_password = args['new_password']
         verify = g.current_user.verify_password(old_password)
         if not verify:
             raise AuthorizedError()
         g.current_user.update(password=new_password)
-        return {}, self.SUCCESS
+        return {}
