f4e042aa32bf447eb8a77b5d9186c394d14a0740
VCong <cong.lv.yx@gmail.com>
2018-11-07 21:25:25 +0800

    fix bug

--- blog/utils/captcha.py
+++ blog/utils/captcha.py
@@ -8,11 +8,11 @@ class FlaskCaptcha:
     CHAR = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 
     def init_app(self, app: Flask):
-        self.fonts_path = app.config['CAPTCHA_FONTS_PATH']
-        self.image_engine = ImageCaptcha(self.fonts_path)
-    
+        self.fonts_path = app.config.get('CAPTCHA_FONTS_PATH') or None
+        self.image_engine = ImageCaptcha(
+            self.fonts_path) if self.fonts_path else ImageCaptcha()
+
     def generate_img(self):
         chars = ''.join(random.choices(self.CHAR, k=4))
         data = self.image_engine.generate(chars)
         return data
-    
