b1c7671b687e29a8e89dc4cfaa11b834984ccf93
CoCong <cong.lv.yx@gmail.com>
2018-12-13 23:59:54 +0800

    delete another garbage code

--- blog/api_v1/auth/permission.py
+++ blog/api_v1/auth/permission.py
@@ -12,23 +12,18 @@ class UserPermission(Resource):
 
     def get(self):
         uid = request.args.get('uid')
-        user = User.query.get(uid)
+        user = User.get(uid)
         if g.current_user == user and g.current_user.can(
                 Permission.ADMINISTER):
             return ''
         raise Forbidden()
 
 
-post_reqparse = reqparse.RequestParser()
-post_reqparse.add_argument('post_id', type=int, location='args', required=True)
-
-
 class PostPermission(Resource):
     decorators = [token_auth.login_required]
 
     def get(self):
-        args = post_reqparse.parse_args()
-        post = Post.query.get(args.post_id)
+        post = Post.query.get(request.args.post_id)
         if g.current_user == post.author or g.current_user.can(
                 Permission.ADMINISTER):
             return {}
